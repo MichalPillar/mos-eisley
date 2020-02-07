1549ebb06d67c0d6260eff68908c447a2fa2c355
Michal Pillar <michal.pillar@gmail.com>
2020-02-07 00:07:47 -0800

    Added directory component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import Header from '../header/header';
+import Header from '../header/Header';
+import Directory from '../directory/Directory';
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Directory />
     </div>
   );
 }
---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Directory.css';
+
+const Directory = () => (
+  <div className="card-list">
+    <h1>Directory test</h1>
+  </div>
+)
+
+export default Directory;
