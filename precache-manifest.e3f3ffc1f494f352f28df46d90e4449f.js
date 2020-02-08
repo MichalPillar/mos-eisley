e7b365d7bbbafde65ec7c69575249ed7467e8a81
Michal Pillar <michal.pillar@gmail.com>
2020-02-07 22:30:01 -0800

    Updates

---
+++ precache-manifest.e3f3ffc1f494f352f28df46d90e4449f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895a10eb11e103f581595b12ed995ad8",
+    "url": "/mos-eisley/index.html"
+  },
+  {
+    "revision": "b83e512915dce090159a",
+    "url": "/mos-eisley/static/css/main.67735a8f.chunk.css"
+  },
+  {
+    "revision": "cb884c94eb45264a9b8f",
+    "url": "/mos-eisley/static/js/2.ae5142af.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mos-eisley/static/js/2.ae5142af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83e512915dce090159a",
+    "url": "/mos-eisley/static/js/main.7fc26eaa.chunk.js"
+  },
+  {
+    "revision": "b8f4c44b367dc88d6467",
+    "url": "/mos-eisley/static/js/runtime-main.d139a967.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/mos-eisley/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
