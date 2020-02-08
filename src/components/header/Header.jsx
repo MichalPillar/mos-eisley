1d8bb91c80e144fab9f1dacbc7f53b8c71c20210
Michal Pillar <michal.pillar@gmail.com>
2020-02-07 22:07:27 -0800

    Added Searchbox and styled it

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Header.css';
 
+import Searchbox from '../searchbox/Searchbox';
+
 const Header = () => (
   <div className="header">
     <h1>Mos Eisley</h1>
+    <Searchbox />
   </div>
 )
 
---
+++ src/components/searchbox/Searchbox.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Searchbox.css';
+
+const Searchbox = () => (
+  <input className="search" type="search" placeholder="Search..." />
+)
+
+export default Searchbox;
